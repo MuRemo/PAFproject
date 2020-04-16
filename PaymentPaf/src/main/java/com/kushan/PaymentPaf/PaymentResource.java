bb95fe20e7a95c1e87c251e70704c50e37f9bd15
“your <kekirideniyakushanravindu@gmail.com>
2020-04-16 19:18:30 +0530

    payment management insert payment

---
+++ PaymentPaf/src/main/java/com/kushan/PaymentPaf/PaymentResource.java
@@ -0,0 +1,44 @@
+package com.kushan.PaymentPaf;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.kushan.PaymentPaf.payment;
+
+@Path("/payments")
+public class PaymentResource {
+payment payobj = new payment();
+@GET
+@Path("/")
+@Produces(MediaType.TEXT_HTML)
+public String readItems()
+ {
+ return "Hello" ;
+ }
+
+
+@POST
+@Path("/")
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+@Produces(MediaType.TEXT_PLAIN)
+public String insertItem(@FormParam("patientid") int patientid,
+ @FormParam("date") String date,
+ @FormParam("cardtype") String cardtype,
+ @FormParam("cardnumber") String cardnumber,
+ @FormParam("expirydate") String expirydate,
+ @FormParam("pinnumber") String pinnumber,
+ @FormParam("amount") String amount)
+{
+ String output = payobj.insertPayment(patientid, date, cardtype, cardnumber, expirydate, pinnumber, amount);
+return output;
+}
+
+}
+
+
+
---
+++ PaymentPaf/src/main/java/model/kushan/PaymentPaf/payment.java
@@ -0,0 +1,53 @@
+package model.kushan.PaymentPaf;
+import java.sql.*;
+
+public class payment {
+	//A common method to connect to the DB
+		private Connection connect()
+		 {
+		 Connection con = null;
+		 try
+		 {
+			 Class.forName("com.mysql.jdbc.Driver");
+			   con = DriverManager.getConnection("jdbc:mysql://localhost/testpaf","root","");
+		 }
+		 catch (Exception e)
+		 {e.printStackTrace();}
+		 return con;
+		 } 
+		
+		public String insertPayment(int patientID, String date, String cardtype, String cardnumber,String expd,String pno,String amount)
+		 {
+		 String output = "";
+		 try
+		 {
+		 Connection con = connect();
+		 if (con == null)
+		 {return "Error while connecting to the database for inserting."; }
+		 // create a prepared statement
+		 String query = "insert into payment(`paymentid`,`patientid`,`date`,`cardtype`,`cardnumber`,`expirydate`,`pinnumber`,`amount`)"+ " values (?, ?, ?, ?, ?, ?, ?, ?)";
+		 PreparedStatement preparedStmt = con.prepareStatement(query);
+		 // binding values
+		 preparedStmt.setInt(1, 0);
+		 preparedStmt.setInt(2, patientID);
+		 preparedStmt.setString(3, date);
+		 preparedStmt.setString(4, cardtype);
+		 preparedStmt.setString(5, cardnumber);
+		 preparedStmt.setString(6, expd);
+		 preparedStmt.setString(7, pno);
+		 preparedStmt.setDouble(8, Double.parseDouble(amount));
+	
+		 
+		//execute the statement
+		preparedStmt.execute();
+		con.close();
+		output = "Inserted successfully";
+		}
+		catch (Exception e)
+		{
+		output = "Error while inserting the item.";
+		System.err.println(e.getMessage());
+		}
+		return output;
+		}
+}
